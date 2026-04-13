6192c5932722675435136efc81fd004ac2068dd6
Shristy <shristy@shristy23.work@gmail.comgit config --listgit config --global user.email shristy@shristy23.work@gmail.com>
2026-04-13 07:36:56 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests # pip install requests
+
+
+
+query = input("What type of news are you interested in today?")
+api = "YOUR_API_KEY"
+
+url = f"https://newsapi.org/v2/everything?q={query}&from=2026-02-03&sortBy=publishedAt&apiKey={api}"
+
+print(url)
+
+r = requests.get(url)
+data = r.json()
+articles = data["articles"]
+
+for index, article in enumerate(articles):
+    print(index + 1, article["title"], article["url"])
+    print("\n****************************\n")
+     
